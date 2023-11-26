4b48ebe29e10c7823563de40f7e653b7051c36b8
Poon Boonchoowit <poon15112@gmail.com>
2023-11-26 17:51:03 +0700

    completed

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,27 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+            Hero h1 = new Hero("woon");
+            Hero h2 = new Hero("poon");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            h1.showcase();
+            h1.equipSword(5);
+            h1.showcase();
+            h1.equipShield(2);
+            h1.showcase();
+
+            h1.levelUp();
+            h1.levelUp();
+            h1.showcase();
+
+            h2.showcase();
+            h2.equipSword(8);
+            h2.showcase();
+            h2.levelUp();
+            h2.showcase();
+            h2.levelUp();
+            h2.levelUp();
+            h2.showcase();
         }
-    }
 }
